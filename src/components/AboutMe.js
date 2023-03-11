8714692ad43f19ae34b50f755eedf28f5425754f
James Aules <james_aules@icloud.com>
2023-03-10 20:21:59 -0500

    creating components

---
+++ src/components/AboutMe.js
@@ -0,0 +1,15 @@
+const AboutMe = () => {
+
+  return (
+    <div >
+        <h1> Jimmy (James) A. Rojas</h1>
+        <p> Have you ever wonder why our people is on such a hurry?
+            or maybe , how is he unverse possible ?
+            Yes?
+            let me explain myself, when i was a kid i was on a Scout group where they taught us how to survive on the forest, how to take care & being responsable with the environment, team work & one of the most 
+            attributes that im glad , being a Lion- Wolf, that means to be able to carry and figure out real-life problems
+        </p>
+    </div>
+  );
+}
+export default AboutMe;
---
+++ src/components/ContactMe.js
@@ -0,0 +1,14 @@
+const ContactMe = () => {
+
+  return (
+    <div >
+       <h1> Contact Me! </h1>
+       <ul>
+          <li>Phone</li>
+          <li>linkendink</li>
+          <li>E-mail</li>
+       </ul>
+    </div>
+  );
+}
+export default ContactMe;
